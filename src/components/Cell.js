4805a327cde5c39612280f885ca456a355891a94
Dylan Stackhouse <Stackhouse.Dylan@gmail.com>
2018-03-01 13:42:10 -0800

    add boolean 'hit' to state, becomes true on click

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,10 +3,20 @@ import React from 'react';
 export class Cell extends React.Component {
   constructor(props) {
     super(props);
-
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      hit: false,
+    }
+  }
+  handleClick(e) {
+    if(!this.state.hit){
+      this.setState({hit: true})
+      console.log(`Hit: ${this.props.x}, ${this.props.y}`);
+    }
   }
 
   render() {
-    return <td className='cell'></td>
+    let style = ['cell', this.state.hit && 'inactive'].join(' ');
+    return <td className={style} onClick={this.handleClick}></td>
   }
 }
