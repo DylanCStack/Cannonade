edadb4e858e36e36e30f595337808b1ed4450430
Dylan Stackhouse <Stackhouse.Dylan@gmail.com>
2018-03-01 13:41:24 -0800

    move grid matrix from Grid to Board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import {Grid} from './Grid.js';
 
+function createGrid(numRows, numCols) {
+  let rows = [];
+  for(let y = 0; y < numRows; y++) {
+    let cols = [];
+    for(let x = 0; x < numCols; x++) {
+      cols.push({'x': x, 'y': y});
+    }
+    rows.push(cols);
+  }
+  return rows;
+}
+
 export class Board extends React.Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      matrix: createGrid(20,20)
+    }
   }
   render() {
-    const dimensions = 20;
+    let matrix = this.state.matrix;
     return(
       <div className='board'>
-        <Grid x={dimensions} y={dimensions}/>
+        <Grid matrix={matrix}/>
       </div>
     );
   }
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,26 +1,16 @@
 import React from 'react';
 import {Cell} from './Cell.js';
 
-function createGrid(numRows, numCols) {
-  let rows = [];
-  for(let y = 0; y < numRows; y++) {
-    let cols = [];
-    for(let x = 0; x < numCols; x++) {
-      cols.push({'x': x, 'y': y});
-    }
-    rows.push(cols);
-  }
-  return rows;
-}
+
 
 export class Grid extends React.Component {
   constructor(props) {
     super(props);
-    this.grid = createGrid(this.props.x, this.props.y);
+
   }
 
   render() {
-    let grid = this.grid.map((row, y) =>{
+    let grid = this.props.matrix.map((row, y) =>{
       return (<tr key={y}>
       {
         row.map((col) => {
