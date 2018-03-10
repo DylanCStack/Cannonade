63604efa6e83ab5197e3e347d7e2cc00efa535f1
Dylan Stackhouse <Stackhouse.Dylan@gmail.com>
2018-03-09 20:30:11 -0800

    add players to app component state. Render boards by number of players

--- src/App.js
+++ src/App.js
@@ -4,11 +4,44 @@ import './App.css';
 import {Board} from './components/Board.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: [
+        {
+          name: 'Player 1',
+          points: 0,
+        },
+        {
+          name: 'Player 2',
+          points: 0,
+        },
+      ],
+    };
+    this.updatePoints = this.updatePoints.bind(this);
+  }
+
+  updatePoints(player) {
+    let players = this.state.players;
+    players[player.id] = player;
+
+    this.setState({players});
+  }
+
   render() {
+    const players = this.state.players.map((player, i) => {
+       player.id = i;
+       return player;
+    });
+    const updatePoints = this.updatePoints;
+
     return (
       <div className="App">
-        <Board player={1}/>
-        <Board player={2}/>
+        {
+          players.map( player => {
+             return <Board player={player} key={player.id} updatePoints={updatePoints} ></Board>;
+          })
+        }
       </div>
     );
   }
